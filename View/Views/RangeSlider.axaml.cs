b323d052049c8cbc713b4da1a443f25fbee90900
FolderMaster <99458522+FolderMaster@users.noreply.github.com>
2024-09-16 14:54:07 +0700

    Rewrite RangeSlider as TemplatedControl.

--- View/Views/RangeSlider.axaml.cs
+++ View/Views/RangeSlider.axaml.cs
@@ -1,162 +1,212 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Metadata;
 using Avalonia.Controls.Primitives;
 using Avalonia.Data;
 using Avalonia.Input;
 using Avalonia.Layout;
-using Avalonia.Media;
+using System;
 
 namespace View.Views
 {
-    public partial class RangeSlider : UserControl
+    [TemplatePart("PART_Panel", typeof(StackPanel))]
+    [TemplatePart("PART_LowerBar", typeof(Control))]
+    [TemplatePart("PART_LowerThumb", typeof(Thumb))]
+    [TemplatePart("PART_MiddleBar", typeof(Control))]
+    [TemplatePart("PART_UpperThumb", typeof(Thumb))]
+    [TemplatePart("PART_UpperBar", typeof(Control))]
+    public partial class RangeSlider : TemplatedControl
     {
-        public static readonly DirectProperty<RangeSlider, double>
-            MinimumProperty = AvaloniaProperty.RegisterDirect<RangeSlider, double>
-            (nameof(Minimum), c => c.Minimum, (c, v) => c.Minimum = v,
-                0, BindingMode.TwoWay);
+        public static readonly StyledProperty<double> MinimumProperty =
+            AvaloniaProperty.Register<RangeSlider, double>(nameof(Minimum), coerce: CoerceMinimum);
 
-        public static readonly DirectProperty<RangeSlider, double>
-            MaximumProperty = AvaloniaProperty.RegisterDirect<RangeSlider, double>
-            (nameof(Maximum), c => c.Maximum, (c, v) => c.Maximum = v,
-                100, BindingMode.TwoWay);
+        public static readonly StyledProperty<double> MaximumProperty =
+            AvaloniaProperty.Register<RangeSlider, double>(nameof(Maximum), coerce: CoerceMaximum);
 
-        public static readonly DirectProperty<RangeSlider, double>
-            LowerValueProperty = AvaloniaProperty.RegisterDirect<RangeSlider, double>
-            (nameof(LowerValue), c => c.LowerValue, (c, v) => c.LowerValue = v,
-                10, BindingMode.TwoWay);
+        public static readonly StyledProperty<double> LowerValueProperty =
+            AvaloniaProperty.Register<RangeSlider, double>(nameof(LowerValue),
+                defaultBindingMode: BindingMode.TwoWay, coerce: CoerceLowerValue);
 
-        public static readonly DirectProperty<RangeSlider, double>
-            UpperValueProperty = AvaloniaProperty.RegisterDirect<RangeSlider, double>
-            (nameof(UpperValue), c => c.UpperValue, (c, v) => c.UpperValue = v,
-                90, BindingMode.TwoWay);
+        public static readonly StyledProperty<double> UpperValueProperty =
+            AvaloniaProperty.Register<RangeSlider, double>(nameof(UpperValue),
+                defaultBindingMode: BindingMode.TwoWay, coerce: CoerceUpperValue);
 
-        public static readonly DirectProperty<RangeSlider, double>
-            MinimumDistanceProperty = AvaloniaProperty.RegisterDirect<RangeSlider, double>
-            (nameof(MinimumDistance), c => c.MinimumDistance, (c, v) => c.MinimumDistance = v,
-                0, BindingMode.TwoWay);
+        public static readonly StyledProperty<double> MinimumDistanceProperty =
+            AvaloniaProperty.Register<RangeSlider, double>(nameof(MinimumDistance),
+                coerce: CoerceMinimumDistance);
+
+        public static readonly StyledProperty<double> BarSizeProperty =
+            AvaloniaProperty.Register<RangeSlider, double>(nameof(BarSize));
 
         public static readonly StyledProperty<Orientation> OrientationProperty =
             AvaloniaProperty.Register<RangeSlider, Orientation>(nameof(Orientation));
 
-        public static readonly StyledProperty<double> BarSizeProperty =
-            AvaloniaProperty.Register<RangeSlider, double>(nameof(BarSize), 50);
+        private StackPanel _panel;
+
+        private Control _lowerBar;
+
+        private Thumb _lowerThumb;
+
+        private Control _middleBar;
+
+        private Thumb _upperThumb;
+
+        private Control _upperBar;
 
         private double _valueToPixelRatio;
 
         private double _pixelToValueRatio;
 
-        private double _minimum = 0;
-
-        private double _maximum = 100;
-
-        private double _lowerValue = 10;
-
-        private double _upperValue = 90;
-
-        private double _minimumDistance = 0;
-
         public double Minimum
         {
-            get => _minimum;
-            set
-            {
-                if (SetAndRaise(MinimumProperty, ref _minimum, value))
-                {
-                    UpdateSize();
-                }
-            }
+            get => GetValue(MinimumProperty);
+            set => SetValue(MinimumProperty, value);
         }
 
         public double Maximum
         {
-            get => _maximum;
-            set
-            {
-                if (SetAndRaise(MaximumProperty, ref _maximum, value))
-                {
-                    UpdateSize();
-                }
-            }
+            get => GetValue(MaximumProperty);
+            set => SetValue(MaximumProperty, value);
         }
 
         public double LowerValue
         {
-            get => _lowerValue;
-            set
-            {
-                if (value < Minimum)
-                {
-                    value = Minimum;
-                }
-                else if (value > UpperValue - MinimumDistance)
-                {
-                    value = UpperValue - MinimumDistance;
-                }
-                if (SetAndRaise(LowerValueProperty, ref _lowerValue, value))
-                {
-                    UpdateLowerBarValue();
-                    UpdateMiddleBarValue();
-                }
-            }
+            get => GetValue(LowerValueProperty);
+            set => SetValue(LowerValueProperty, value);
         }
 
         public double UpperValue
         {
-            get => _upperValue;
-            set
-            {
-                if (value > Maximum)
-                {
-                    value = Maximum;
-                }
-                else if (value < LowerValue + MinimumDistance)
-                {
-                    value = LowerValue + MinimumDistance;
-                }
-                if (SetAndRaise(UpperValueProperty, ref _upperValue, value))
-                {
-                    UpdateMiddleBarValue();
-                    UpdateUpperBarValue();
-                }
-            }
+            get => GetValue(UpperValueProperty);
+            set => SetValue(UpperValueProperty, value);
         }
 
         public double MinimumDistance
         {
-            get => _minimumDistance;
-            set
-            {
-                SetAndRaise(MinimumDistanceProperty, ref _minimumDistance, value);
-            }
+            get => GetValue(MinimumDistanceProperty);
+            set => SetValue(MinimumDistanceProperty, value);
         }
 
         public Orientation Orientation
         {
             get => GetValue(OrientationProperty);
-            set
-            {
-                SetValue(OrientationProperty, value);
-                UpdateSize();
-            }
+            set => SetValue(OrientationProperty, value);
         }
 
         public double BarSize
         {
             get => GetValue(BarSizeProperty);
-            set
-            {
-                SetValue(BarSizeProperty, value);
-                UpdateBarsSizes();
-            }
+            set => SetValue(BarSizeProperty, value);
         }
 
         static RangeSlider()
         {
+            MinimumProperty.Changed.AddClassHandler<RangeSlider, double>
+                ((o, e) => o.OnMinimumChanged(e));
+            MaximumProperty.Changed.AddClassHandler<RangeSlider, double>
+                ((o, e) => o.OnMaximumChanged(e));
+            LowerValueProperty.Changed.AddClassHandler<RangeSlider, double>
+                ((o, e) => o.OnLowerValueChanged(e));
+            UpperValueProperty.Changed.AddClassHandler<RangeSlider, double>
+                ((o, e) => o.OnUpperValueChanged(e));
+            OrientationProperty.Changed.AddClassHandler<RangeSlider, Orientation>
+                ((o, e) => o.OnOrientationChanged(e));
         }
 
-        public RangeSlider()
+        protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
         {
-            InitializeComponent();
+            base.OnApplyTemplate(e);
+            _panel = e.NameScope.Find<StackPanel>("PART_Panel");
+            _panel.SizeChanged += Panel_SizeChanged;
+            _lowerBar = e.NameScope.Find<Control>("PART_LowerBar");
+            _lowerBar.PointerPressed += LowerBar_PointerPressed;
+            _lowerThumb = e.NameScope.Find<Thumb>("PART_LowerThumb");
+            _lowerThumb.DragDelta += LowerThumb_DragDelta;
+            _middleBar = e.NameScope.Find<Control>("PART_MiddleBar");
+            _upperThumb = e.NameScope.Find<Thumb>("PART_UpperThumb");
+            _upperThumb.DragDelta += UpperThumb_DragDelta;
+            _upperBar = e.NameScope.Find<Control>("PART_UpperBar");
+            _upperBar.PointerPressed += UpperBar_PointerPressed;
+        }
+
+        private static double CoerceMinimum(AvaloniaObject sender, double value)
+        {
+            var rangeSlider = (RangeSlider)sender;
+            return ValidateDouble(value) ? Math.Min(value,
+                rangeSlider.LowerValue) : rangeSlider.Minimum;
+        }
+
+        private static double CoerceMaximum(AvaloniaObject sender, double value)
+        {
+            var rangeSlider = (RangeSlider)sender;
+            return ValidateDouble(value) ? Math.Max(value,
+                rangeSlider.UpperValue) : rangeSlider.Maximum;
+        }
+
+        private static double CoerceLowerValue(AvaloniaObject sender, double value)
+        {
+            var rangeSlider = (RangeSlider)sender;
+            return ValidateDouble(value) ? Math.Clamp(value, rangeSlider.Minimum,
+                rangeSlider.UpperValue - rangeSlider.MinimumDistance) : rangeSlider.LowerValue;
+        }
+
+        private static double CoerceUpperValue(AvaloniaObject sender, double value)
+        {
+            var rangeSlider = (RangeSlider)sender;
+            return ValidateDouble(value) ? Math.Clamp(value, rangeSlider.LowerValue +
+                rangeSlider.MinimumDistance, rangeSlider.Maximum) : rangeSlider.UpperValue;
+        }
+
+        private static double CoerceMinimumDistance(AvaloniaObject sender, double value)
+        {
+            var rangeSlider = (RangeSlider)sender;
+            return ValidateDouble(value) ? Math.Clamp(value, 0, rangeSlider.UpperValue -
+                rangeSlider.LowerValue) : rangeSlider.MinimumDistance;
+        }
+
+        private static bool ValidateDouble(double value) =>
+            !double.IsInfinity(value) && !double.IsNaN(value);
+
+        private void OnMinimumChanged(AvaloniaPropertyChangedEventArgs<double> args)
+        {
+            if (IsInitialized)
+            {
+                UpdateSize();
+            }
+        }
+
+        private void OnMaximumChanged(AvaloniaPropertyChangedEventArgs<double> args)
+        {
+            if (IsInitialized)
+            {
+                UpdateSize();
+            }
+        }
+
+        private void OnLowerValueChanged(AvaloniaPropertyChangedEventArgs<double> args)
+        {
+            if (IsInitialized)
+            {
+                UpdateLowerBarValue();
+                UpdateMiddleBarValue();
+            }
+        }
+
+        private void OnUpperValueChanged(AvaloniaPropertyChangedEventArgs<double> args)
+        {
+            if (IsInitialized)
+            {
+                UpdateMiddleBarValue();
+                UpdateUpperBarValue();
+            }
+        }
+
+        private void OnOrientationChanged(AvaloniaPropertyChangedEventArgs<Orientation> args)
+        {
+            if (IsInitialized)
+            {
+                UpdateSize();
+            }
         }
 
         private void UpdateBarValue(Control bar, double lowerValue, double upperValue)
@@ -172,44 +222,46 @@ namespace View.Views
             }
         }
 
+        private void UpdateLowerBarValue() =>
+            UpdateBarValue(_lowerBar, Minimum, LowerValue);
+
+        private void UpdateMiddleBarValue() =>
+            UpdateBarValue(_middleBar, LowerValue, UpperValue);
+
+        private void UpdateUpperBarValue() =>
+            UpdateBarValue(_upperBar, UpperValue, Maximum);
+
         private void UpdateBarsSizes()
         {
             if (Orientation == Orientation.Horizontal)
             {
-                upperBar.Height = BarSize;
-                middleBar.Height = BarSize;
-                lowerBar.Height = BarSize;
+                _upperBar.Height = BarSize;
+                _middleBar.Height = BarSize;
+                _lowerBar.Height = BarSize;
             }
             else
             {
-                upperBar.Width = BarSize;
-                middleBar.Width = BarSize;
-                lowerBar.Width = BarSize;
+                _upperBar.Width = BarSize;
+                _middleBar.Width = BarSize;
+                _lowerBar.Width = BarSize;
             }
         }
-
-        private void UpdateLowerBarValue() => UpdateBarValue(lowerBar, Minimum, LowerValue);
-
-        private void UpdateMiddleBarValue() => UpdateBarValue(middleBar, LowerValue, UpperValue);
-
-        private void UpdateUpperBarValue() => UpdateBarValue(upperBar, UpperValue, Maximum);
 
         private void UpdateSize()
         {
             if (Orientation == Orientation.Horizontal)
             {
-                _pixelToValueRatio = (stack.Bounds.Width - (lowerThumb.Bounds.Width +
-                    upperThumb.Bounds.Width)) / (Maximum - Minimum);
-                _valueToPixelRatio = (Maximum - Minimum) / (stack.Bounds.Width -
-                    (lowerThumb.Bounds.Width + upperThumb.Bounds.Width));
-                
+                _pixelToValueRatio = (_panel.Bounds.Width - (_lowerThumb.Bounds.Width +
+                    _upperThumb.Bounds.Width)) / (Maximum - Minimum);
+                _valueToPixelRatio = (Maximum - Minimum) / (_panel.Bounds.Width -
+                    (_lowerThumb.Bounds.Width + _upperThumb.Bounds.Width));
             }
             else
             {
-                _pixelToValueRatio = (stack.Bounds.Height - (lowerThumb.Bounds.Height +
-                    upperThumb.Bounds.Height)) / (Maximum - Minimum);
-                _valueToPixelRatio = (Maximum - Minimum) / (stack.Bounds.Height -
-                    (lowerThumb.Bounds.Height + upperThumb.Bounds.Height));
+                _pixelToValueRatio = (_panel.Bounds.Height - (_lowerThumb.Bounds.Height +
+                    _upperThumb.Bounds.Height)) / (Maximum - Minimum);
+                _valueToPixelRatio = (Maximum - Minimum) / (_panel.Bounds.Height -
+                    (_lowerThumb.Bounds.Height + _upperThumb.Bounds.Height));
             }
             UpdateBarsSizes();
             UpdateLowerBarValue();
@@ -226,26 +278,26 @@ namespace View.Views
             double value;
             if(Orientation == Orientation.Horizontal)
             {
-                if (sender == lowerBar)
+                if (sender == _lowerBar)
                 {
-                    value = lowerBar.Bounds.Width +
-                        lowerThumb.Bounds.Width / 2 - point.X;
+                    value = _lowerBar.Bounds.Width +
+                        _lowerThumb.Bounds.Width / 2 - point.X;
                 }
                 else
                 {
-                    value = point.X + upperThumb.Bounds.Width / 2;
+                    value = point.X + _upperThumb.Bounds.Width / 2;
                 }
             }
             else
             {
-                if (sender == lowerBar)
+                if (sender == _lowerBar)
                 {
-                    value = lowerBar.Bounds.Height +
-                        lowerThumb.Bounds.Height / 2 - point.Y;
+                    value = _lowerBar.Bounds.Height +
+                        _lowerThumb.Bounds.Height / 2 - point.Y;
                 }
                 else
                 {
-                    value = point.Y + upperThumb.Bounds.Height / 2;
+                    value = point.Y + _upperThumb.Bounds.Height / 2;
                 }
             }
             return _valueToPixelRatio * value;
@@ -263,6 +315,6 @@ namespace View.Views
         private void UpperBar_PointerPressed(object sender, PointerPressedEventArgs e) =>
             UpperValue += GetChangeValueByBarPoint(sender, e);
 
-        private void Stack_SizeChanged(object? sender, SizeChangedEventArgs e) => UpdateSize();
+        private void Panel_SizeChanged(object? sender, SizeChangedEventArgs e) => UpdateSize();
     }
 }
